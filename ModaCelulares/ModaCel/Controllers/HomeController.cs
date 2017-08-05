0362ef1f10cc4df72716f1b6b10fa474d652e0e7
patykdo222 <30646239+patykdo222@users.noreply.github.com>
2017-08-04 21:14:44 -0500

    version1
    version 1 de proyecto final

---
+++ ModaCelulares/ModaCel/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ModaCel.Controllers
+{
+    public class HomeController : Controller
+    {
+        private Models.BDCelularesEntities bd = new Models.BDCelularesEntities();
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Buscar(string id="")
+        {
+            //logica acceso a BD
+            var productos = bd.Producto
+                .Where(x => x.Denominacion.Contains(id))
+                .Take(10)
+                .ToList();
+            //devuelve lista de productos
+            ViewBag.clave = id;
+            return View(productos);
+        }
+    }
+}
---
+++ ModaCelulares/ModaCel/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ModaCel.MvcApplication" Language="C#" %>
